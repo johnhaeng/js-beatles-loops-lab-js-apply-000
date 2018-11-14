7fab8e8e67fb323739010c2c11da35f6e56ec410
inherited-firmware-6870 <johnhaeng@gmail.com>
2018-11-14 23:00:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // add solution here
+
+function theBeatlesPlay (arrMusic, arrInstr) {
+  var arr = [];
+  
+  for (var i = 0; i < arrMusic.length; i += 1) {
+    var musicians = arrMusic[i];
+    
+    
+  }
+} 
